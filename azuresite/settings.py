03988b9e85f716542dde75ce78191cd038ff8635
SanthoshRaj Murugesan <santhoshraj2960@hotmail.com>
2021-02-11 16:46:23 -0600

    integerated code coverage tool

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -39,6 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
+]
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Tell nose to measure coverage on the 'foo' and 'bar' apps
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=polls',
+    '--cover-html',
 ]
 
 MIDDLEWARE = [
