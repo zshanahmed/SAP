6cd3b59925f000e03e9b0d17c8e0bd8cc7b515fd
Nam Hoang Le <nam-h-le@uiowa.edu>
2021-02-17 15:21:04 -0600

    Testes github configuration

---
+++ azuresite/ally_login.py
@@ -0,0 +1,18 @@
+"""
+Feature name: Ally Login Page
+Author: Nam H. Le
+
+"""
+
+
+from django.urls import path
+
+
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
