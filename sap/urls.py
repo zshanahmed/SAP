d2bb785aaf9272b5168cf67b68817aaa16af938e
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-02-18 10:46:39 -0600

    Added login logout views and changed redirect URLs for login logout

--- sap/urls.py
+++ sap/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='sap-home'),
+    path('', auth_views.LoginView.as_view(template_name='sap/login.html'), name='home'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='sap/logout.html'), name='logout'),
     path('dashboard/', views.dashboard, name='sap-dasboard'),
 ]
