b685d6036532e8e19307d292890c27fafe161b21
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-02-19 19:49:58 -0600

    Updated conftest to use options instead of chrome_options and added login figma design

--- sap/views.py
+++ sap/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'sap/home.html')
-
 def dashboard(request):
     return render(request, 'sap/dashboard.html')
--- tests/step_definitions/conftest.py
+++ tests/step_definitions/conftest.py
@@ -5,6 +5,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 @pytest.fixture
 def firefoxBrowser():
     b = webdriver.Firefox()
