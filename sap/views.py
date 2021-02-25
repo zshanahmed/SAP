44adecca1684c4b82a0356612d4cfdd61013f416
eshaeffer <eshaeffer@uiowa.edu>
2021-02-25 01:32:30 -0600

    Added get/post methods for createIBAadmin view. Added alert at top of dashboard page.

--- sap/views.py
+++ sap/views.py
@@ -1,6 +1,11 @@
 from .models import Ally
 from django.views import generic
 from django.views.generic import TemplateView
+from django.views.generic import FormView
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
@@ -21,3 +26,11 @@ class AdminProfileView(TemplateView):
 
 class CreateAdminView(TemplateView):
     template_name = "sap/create_iba_admin.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        print(request.POST)
+        messages.add_message(request, messages.SUCCESS, 'Account Created')
+        return redirect('/dashboard')
