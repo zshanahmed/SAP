ade410142f501e47b1ac0f956e0b1fa8a9c4f7d5
eshaeffer <eshaeffer@uiowa.edu>
2021-02-26 17:51:32 -0600

    Added link to create_iba_admin to iba logo that goes to dashboard. Added divider for new dropdown item.

--- sap/urls.py
+++ sap/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     url('analytics/',
         login_required(views.AnalyticsView.as_view(), login_url='home'),
         name='sap-analytics'),
-    url(r'forgot-password/$', views.ForgotPasswordView.as_view(), name='forgot_password')
+    url(r'forgot-password/$', views.ForgotPasswordView.as_view(), name='forgot_password'),
 
     url('create_iba_admin/',
         login_required(views.CreateAdminView.as_view(), login_url='home'),
