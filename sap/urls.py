1d0b126e0712ad248930f372b2b830a1334da63b
Asad Mahmood <asad1996172@hotmail.com>
2021-02-26 08:01:55 -0600

    Added Change Password and Update Profile

--- sap/urls.py
+++ sap/urls.py
@@ -8,16 +8,17 @@ app_name = 'sap'
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='sap/login.html'), name='home'),
     path('logout/', views.logout_request, name='logout'),
-
+    
+    url(r'^password/$', login_required(views.change_password), name='change_password'),
+    url(r'^update_profile/$', login_required(views.edit_admin_profile),
+        name='sap-admin_profile'),
     url(r'^dashboard/$',
         login_required(views.AlliesListView.as_view(), login_url='home'),
         name='sap-dashboard'),
     url('analytics/',
         login_required(views.AnalyticsView.as_view(), login_url='home'),
         name='sap-analytics'),
-    url('update_profile/',
-        login_required(views.AdminProfileView.as_view(), login_url='home'),
-        name='sap-admin_profile'),
+    
     url(r'forgot-password/$', views.ForgotPasswordView.as_view(), name='forgot_password')
 
 ]
--- sap/views.py
+++ sap/views.py
@@ -1,15 +1,54 @@
 from django.contrib.auth import logout
-from django.shortcuts import redirect
 from .models import Ally
 from django.views import generic
 from django.views.generic import TemplateView
 from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
 def logout_request(request):
     logout(request)
     return redirect('sap:home')
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(
+                request, 'Password Updated Successfully !')
+            return redirect('sap:change_password')
+        else:
+            messages.error(request, "Couldn't Update Password !")
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'sap/change_password.html', {
+        'form': form
+    })
+
+
+def edit_admin_profile(request):
+    if request.method == 'POST':
+        curr_user = request.user
+        new_username = request.POST.get('username')
+        new_email = request.POST.get('email')
+        if not User.objects.filter(username=new_username).exists():
+            curr_user.username = new_username
+            curr_user.email = new_email
+            curr_user.save()
+            messages.success(request, "Profile Updated !")
+            return redirect('sap:sap-admin_profile')
+        else:
+            messages.error(request, "Couldn't Update Profile ! Username already exists")
+    
+    return render(request, 'sap/profile.html')
 
 class AlliesListView(generic.ListView):
     template_name = 'sap/dashboard.html'
