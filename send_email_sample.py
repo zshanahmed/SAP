39da9710276170a87d5db5990ed31653b837d32e
Nam Hoang Le <nam-h-le@uiowa.edu>
2021-03-04 23:16:18 -0600

    A way to get Sendgrid API key via environment variable

--- send_email_sample.py
+++ send_email_sample.py
@@ -1,15 +1,12 @@
 # using SendGrid's Python Library
 # https://github.com/sendgrid/sendgrid-python
 import os
-import json
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 # THIS IS THE MINIMUM NEEDED CODE TO SEND AN EMAIL
 # RUN THIS CODE TO VERIFY THE INTEGRATION WORKS CORRECTLY
 
-with open('./config.json') as f:
-  config = json.load(f)
 
 message = Mail(
     from_email='nam-h-le@uiowa.edu', # this email address must be in Sendgrid's sender list OR it must have a whitelisted domain name
@@ -18,7 +15,7 @@ message = Mail(
     html_content='<strong>This is an email from Science Alliance Portal</strong>')
 try:
     # sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-    sg = SendGridAPIClient(config['sendgrid_web_API'])
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sg.send(message)
     print(response.status_code)
     print(response.body)
