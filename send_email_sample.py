0e277f8adacac45c501367421cb0ee3751471ab7
Nam Hoang Le <nam-h-le@uiowa.edu>
2021-03-04 23:28:14 -0600

    Comment removed

--- send_email_sample.py
+++ send_email_sample.py
@@ -14,7 +14,6 @@ message = Mail(
     subject='Sample Email From Science Alliance Portal',
     html_content='<strong>This is an email from Science Alliance Portal</strong>')
 try:
-    # sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sg.send(message)
     print(response.status_code)
