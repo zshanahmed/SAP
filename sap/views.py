6904431589e999fac6bad1e00f7c1eaae66d7152
Nam Hoang Le <nam-h-le@uiowa.edu>
2021-03-12 19:12:30 -0600

    Update views.py

--- sap/views.py
+++ sap/views.py
@@ -354,8 +354,8 @@ class ForgotPasswordView(TemplateView):
                     html_content=message_body)
 
                 try:
-                    # sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-                    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+#                     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+                    sg = SendGridAPIClient('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')
                     response = sg.send(email_content)
                     print(response.status_code)
                     print(response.body)
