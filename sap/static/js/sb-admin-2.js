27ff2d7d33d5c06ee9920dbe0e0e65dcf17bf226
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-03-24 00:29:56 -0500

    Updated style changes for choose file and added alert on selected file

--- sap/static/js/sb-admin-2.js
+++ sap/static/js/sb-admin-2.js
@@ -75,4 +75,11 @@
       .find(".modal-footer a")
       .attr("href", "/delete/?username=" + recipient);
   });
+
+  $('input[type="file"]').change(function(e){
+            var fileName = e.target.files[0].name;
+            console.log(fileName);
+            alert('The file "' + fileName +  '" has been selected.');
+  });
+
 })(jQuery); // End of use strict
