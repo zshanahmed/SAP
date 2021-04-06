a132f8bde4f42039e290b617351b52f9a812b4c5
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-04-06 17:04:17 -0500

    Disabling functionality to add events on HTML DOM

--- sap/static/js/calendar.js
+++ sap/static/js/calendar.js
@@ -62,46 +62,46 @@ $(document).ready(function() {
             day: 'MMMM yyyy'                  // Tuesday, Sep 8, 2009
         },
         allDaySlot: false,
-        selectHelper: true,
-        select: function(start, end, allDay) {
-            var title = prompt('Event Title:');
-            if (title) {
-                calendar.fullCalendar('renderEvent',
-                    {
-                        title: title,
-                        start: start,
-                        end: end,
-                        allDay: allDay
-                    },
-                    true // make the event "stick"
-                );
-            }
-            calendar.fullCalendar('unselect');
-        },
-            droppable: true, // this allows things to be dropped onto the calendar !!!
-            drop: function(date, allDay) { // this function is called when something is dropped
-
-                // retrieve the dropped element's stored Event Object
-                var originalEventObject = $(this).data('eventObject');
-
-                // we need to copy it, so that multiple events don't have a reference to the same object
-                var copiedEventObject = $.extend({}, originalEventObject);
-
-                // assign it the date that was reported
-                copiedEventObject.start = date;
-                copiedEventObject.allDay = allDay;
-
-                // render the event on the calendar
-                // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
-                $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
-
-                // is the "remove after drop" checkbox checked?
-                if ($('#drop-remove').is(':checked')) {
-                    // if so, remove the element from the "Draggable Events" list
-                    $(this).remove();
-                }
-
-            },
+        // selectHelper: true,
+        // select: function(start, end, allDay) {
+        //     var title = prompt('Event Title:');
+        //     if (title) {
+        //         calendar.fullCalendar('renderEvent',
+        //             {
+        //                 title: title,
+        //                 start: start,
+        //                 end: end,
+        //                 allDay: allDay
+        //             },
+        //             true // make the event "stick"
+        //         );
+        //     }
+        //     calendar.fullCalendar('unselect');
+        // },
+        //     droppable: true, // this allows things to be dropped onto the calendar !!!
+        //     drop: function(date, allDay) { // this function is called when something is dropped
+        //
+        //         // retrieve the dropped element's stored Event Object
+        //         var originalEventObject = $(this).data('eventObject');
+        //
+        //         // we need to copy it, so that multiple events don't have a reference to the same object
+        //         var copiedEventObject = $.extend({}, originalEventObject);
+        //
+        //         // assign it the date that was reported
+        //         copiedEventObject.start = date;
+        //         copiedEventObject.allDay = allDay;
+        //
+        //         // render the event on the calendar
+        //         // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+        //         $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
+        //
+        //         // is the "remove after drop" checkbox checked?
+        //         if ($('#drop-remove').is(':checked')) {
+        //             // if so, remove the element from the "Draggable Events" list
+        //             $(this).remove();
+        //         }
+        //
+        //     },
 
         events: [
             {
