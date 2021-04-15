9e318cce16751ce40871b024f3c676afb38dc376
Nam Hoang Le <nam-h-le@uiowa.edu>
2021-04-15 14:32:06 -0500

    Dispatch method for CustomLogin

--- sap/views.py
+++ sap/views.py
@@ -55,6 +55,20 @@ def logout_request(request):
 
 
 class CustomLoginView(auth_views.LoginView):
+    redirect_field_name = REDIRECT_FIELD_NAME
+    template_name = 'registration/login.html'
+    redirect_authenticated_user = False
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.redirect_authenticated_user and self.request.user.is_authenticated:
+            redirect_to = self.get_success_url()
+            if redirect_to == self.request.path:
+                raise ValueError(
+                    "Redirection loop for authenticated user detected. Check that "
+                    "your LOGIN_REDIRECT_URL doesn't point to a login page."
+                )
+            return HttpResponseRedirect(redirect_to)
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
 
