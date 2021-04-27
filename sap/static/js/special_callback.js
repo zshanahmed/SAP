f9ba3fad4873d2354d904667e40bcb872a54c8d1
eshaeffer <eshaeffer@uiowa.edu>
2021-04-26 20:48:05 -0500

    Created a custom callback that fills the notification list with <a> tags and only returns the message. (other notification list was uber ugly.)

---
+++ sap/static/js/special_callback.js
@@ -0,0 +1,16 @@
+function custom_fill_notification_list(data) {
+    var menus = document.getElementsByClassName(notify_menu_class);
+    if (menus) {
+        var messages = data.unread_list.map(function (item) {
+            var message = "";
+            if(typeof item.verb !== 'undefined'){
+                message = message + " " + item.verb;
+            }
+            return '<a class="dropdown-item" type="button">' + message + '</a>';
+        }).join('')
+
+        for (var i = 0; i < menus.length; i++){
+            menus[i].innerHTML = messages;
+        }
+    }
+}
