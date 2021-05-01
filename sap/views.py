142fbaca3b6bf43ef1004e6bd53bd133163b2108
eshaeffer <eshaeffer@uiowa.edu>
2021-04-30 22:14:30 -0500

    fixed notifications to only count the announcements and event notifications to delete if > 10. Checks if action object is same to prevent duplicate notifications from appearing when editing events.

--- sap/views.py
+++ sap/views.py
@@ -42,9 +42,16 @@ def make_notification(request, notifications, user, msg, action_object=''):
     @action_object: django object (optional)
     """
     if notifications.exists():
-        length = len(notifications)
+        announcements_and_events = []
+        for notification in notifications:
+            if notification.action_object == action_object:
+                notification.delete()
+            elif notification.action_object._meta.verbose_name == 'event' or \
+                notification.action_object._meta.verbose_name == 'announcement':
+                announcements_and_events.append(notification)
+        length = len(announcements_and_events)
         while length >= 10:
-            notifications[length - 1].delete()
+            announcements_and_events[length - 1].delete()
             length -= 1
     if action_object == '':
         notify.send(request.user, recipient=user, verb=msg)
