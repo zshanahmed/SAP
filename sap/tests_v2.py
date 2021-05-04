50024847c1af49c8d02bc79c08f8fe064a7a78f6
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-05-04 16:01:37 -0500

    Fixed linting issues

--- sap/tests_v2.py
+++ sap/tests_v2.py
@@ -8,12 +8,11 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase, Client  # tests file
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
-from django.contrib.auth.forms import PasswordResetForm
-from .forms import UserResetForgotPasswordForm
+
 from .models import Ally, StudentCategories, AllyStudentCategoryRelation, Event, EventInviteeRelation
 
 # Create your tests here.
-from .tokens import password_reset_token, account_activation_token
+from .tokens import account_activation_token
 
 User = get_user_model()
 
@@ -23,11 +22,11 @@ class SignUpTests(TestCase):
     """
 
     def setUp(self):
-        self.username = 'admin'
-        self.username_active = 'user_active'
-        self.password = 'admin_password1'
-        self.email = 'email@test.com'
-        self.email_active = 'email_active@test.com'
+        self.username = 'admin2'
+        self.username_active = 'user_active2'
+        self.password = 'admin_password21'
+        self.email = 'email2@test.com'
+        self.email_active = 'email_active2@test.com'
         self.user = User.objects.create_user(username=self.username, email=self.email, password=self.password)
         self.user_active = User.objects.create_user(username=self.username_active, email=self.email_active, password=self.password,
                                                     is_active=True)
@@ -757,222 +756,18 @@ class SignUpTests(TestCase):
         request = self.client.get(link)
         self.assertEqual(request.status_code, HTTPStatus.FOUND)
 
-
-class ForgotPasswordTest(TestCase):
-    """
-    Unit tests for forgot password feature
-    """
-
-    def setUp(self):
-        self.username = 'user1'
-        self.password = 'user_password1'
-        self.email = 'email1@test.com'
-        self.client = Client()
-        self.user = User.objects.create_user(self.username, self.email, self.password)
-
-    def test_get_success(self):
-        """
-        Can access if user is not logged in.
-        """
-        self.client.logout()
-        response = self.client.get(reverse('sap:password-forgot'))
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertContains(
-            response, "Send me instructions!", html=True
-        )
-
-    def test_get_fail(self):
-        """
-        Cannot access if user is not logged in.
-        """
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.get(reverse('sap:password-forgot'))
-        self.assertEqual(response.status_code, HTTPStatus.FOUND)
-
-    def test_enter_valid_reset_email_address(self):
-        """
-        Enter email address and receive a message
-        """
-        response = self.client.get(reverse('sap:password-forgot'))
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertContains(
-            response, "Send me instructions!", html=True
-        )
-
-        data = {
-            "email": "email1@test.com",
-        }
-        form = PasswordResetForm(
-            data=data
-        )
-        self.assertTrue(form.is_valid())
-
-        response = self.client.post(
-            reverse("sap:password-forgot"), data=data, follow=True)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_enter_invalid_reset_email_address(self):
-        """
-        Enter invalid email address and receive a message.
-        """
-        response = self.client.get(reverse('sap:password-forgot'))
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertContains(
-            response, "Send me instructions!", html=True
-        )
-
-        data = {
-            "email": "wrongemail@test.com",
-        }
-        form = PasswordResetForm(
-            data=data
-        )
-        self.assertTrue(form.is_valid())
-
-        response = self.client.post(
-            reverse('sap:password-forgot'), data=data, follow=True)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_enter_invalid_form(self):
-        """
-        Enter invalid text in email field.
-        """
-        response = self.client.get(reverse('sap:password-forgot'))
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertContains(
-            response, "Send me instructions!", html=True
-        )
-
-        data = {
-            "email": "not_an_email_address",
-        }
-        form = PasswordResetForm(
-            data=data
-        )
-        self.assertFalse(form.is_valid())
-
-        response = self.client.post(
-            reverse('sap:password-forgot'), data=data, follow=True)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_if_confirmation_link_work(self):
-        """
-        The unique link to reset password exists and works
-        """
-        token = password_reset_token.make_token(self.user)
-        uid = urlsafe_base64_encode(force_bytes(self.user.pk))
-        link = reverse('sap:password-forgot-confirm', args=[uid, token])
-
-        request = self.client.get(link)
-        self.assertEqual(request.status_code, HTTPStatus.OK)
-
-    def test_reset_password_success(self):
-        """
-        Successfully create new password.
-        """
-        token = password_reset_token.make_token(self.user)
-        uid = urlsafe_base64_encode(force_bytes(self.user.pk))
-        link = reverse('sap:password-forgot-confirm', args=[uid, token])
-
-        request = self.client.get(link)
-        self.assertEqual(request.status_code, HTTPStatus.OK)
-
-        data = {
-            "new_password1": "user_password2",
-            "new_password2": "user_password2"
-        }
-        form = UserResetForgotPasswordForm(
-            user=self.user,
-            data=data
-        )
-        self.assertTrue(form.is_valid())
-
-        response = self.client.post(
-            link, data=data, follow=True)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        message = list(response.context['messages'])[0]
-        self.assertEqual(message.message, "New Password Created Successfully!")
-
-    def test_reset_password_failure(self):
-        """
-        Fail to create new password.
-        """
-        token = password_reset_token.make_token(self.user)
-        uid = urlsafe_base64_encode(force_bytes(self.user.pk))
-        link = reverse('sap:password-forgot-confirm', args=[uid, token])
-
-        request = self.client.get(link)
-        self.assertEqual(request.status_code, HTTPStatus.OK)
-
-        data = {
-            "new_password1": "user_password2",
-            "new_password2": "wrong_password"
-        }
-        form = UserResetForgotPasswordForm(
-            user=self.user,
-            data=data
-        )
-        self.assertFalse(form.is_valid())
-
-        response = self.client.post(
-            link, data=data, follow=True)
-        # self.assertEqual(response.url, link)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_confirmation_link_fail_for_get_method(self):
-        """
-        Cannot open invalid confirmation link.
-        """
-        token = password_reset_token.make_token(self.user)
-        uid = urlsafe_base64_encode(force_bytes(self.user.pk))
-        link = reverse('sap:password-forgot-confirm', args=[uid, token])
-
-        self.user.delete()
-
-        request = self.client.get(link)
-        self.assertEqual(request.status_code, HTTPStatus.FOUND)
-
-    def test_confirmation_link_fail_for_post_method(self):
-        """
-        Fail to create new password.
-        """
-        token = password_reset_token.make_token(self.user)
-        uid = urlsafe_base64_encode(force_bytes(self.user.pk))
-        link = reverse('sap:password-forgot-confirm', args=[uid, token])
-
-        request = self.client.get(link)
-        self.assertEqual(request.status_code, HTTPStatus.OK)
-
-        data = {
-            "new_password1": "user_password2",
-            "new_password2": "user_password2"
-        }
-        form = UserResetForgotPasswordForm(
-            user=self.user,
-            data=data
-        )
-        self.assertTrue(form.is_valid())
-
-        self.user.delete()
-
-        response = self.client.post(
-            link, data=data, follow=True)
-        # self.assertEqual(response.url, link)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-
 class SignUpDoneViewTests(TestCase):
     """
     Unit tests for SignUpDoneView
     """
     def setUp(self):
-        self.username = 'admin'
-        self.username_active = 'user_active'
-        self.password = 'admin_password1'
-        self.email = 'email@test.com'
-        self.email_active = 'email_active@test.com'
+        self.username = 'admin1'
+        self.username_active = 'user_active1'
+        self.password = 'admin_password12'
+        self.email = 'email1@test.com'
+        self.active_email = 'email_active1@test.com'
         self.user = User.objects.create_user(username=self.username, email=self.email, password=self.password)
-        self.user_active = User.objects.create_user(username=self.username_active, email=self.email_active,
+        self.user_active = User.objects.create_user(username=self.username_active, email=self.active_email,
                                                     password=self.password,
                                                     is_active=True)
         self.client = Client()
@@ -1026,37 +821,4 @@ class SignUpDoneViewTests(TestCase):
         """
         self.client.login(username=self.username, password=self.password)
         response = self.client.get(reverse('sap:sign-up-done'))
-        self.assertEqual(response.status_code, 302)
-
-
-class ForgotPasswordDoneView(TestCase):
-    """
-    Unit tests for SignUpDoneView
-    """
-    def setUp(self):
-        self.username = 'admin'
-        self.username_active = 'user_active'
-        self.password = 'admin_password1'
-        self.email = 'email@test.com'
-        self.email_active = 'email_active@test.com'
-        self.user = User.objects.create_user(username=self.username, email=self.email, password=self.password)
-        self.user_active = User.objects.create_user(username=self.username_active, email=self.email_active,
-                                                    password=self.password,
-                                                    is_active=True)
-        self.client = Client()
-
-    def test_forgotpassword_if_keyerror(self):
-        """
-        If keyerror
-        """
-        self.client.logout()
-        response = self.client.get(reverse('sap:password-forgot-done'))
-        self.assertEqual(response.status_code, 302)
-
-    def test_forgotpassword_if_keyerror_and_is_authenticated(self):
-        """
-        If keyerror and is_authenticated
-        """
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.get(reverse('sap:password-forgot-done'))
         self.assertEqual(response.status_code, 302)
